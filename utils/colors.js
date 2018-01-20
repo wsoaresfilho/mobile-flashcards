b749da6ce9f204c5c4cd6a9f789acf49c9b21eaa
Washington Soares <wsoaresfilho@hotmail.com.br>
2018-01-20 20:00:30 -0200

    feat: Creating colors file

---
+++ utils/colors.js
@@ -0,0 +1,8 @@
+// Commom colors for the app
+export const gray = '#767676'
+export const white = '#fff'
+export const red = '#d4271b'
+export const yellow = '#ffeb3b'
+export const blue = '#4e4cb8'
+export const black = '#000'
+export const green = '#008000'
